ac20818cb8c23af75ee25b4306da037422874c33
gugu <gustavocorrea2032@gmail.com>
2019-07-02 11:34:13 -0300

    Identificando o maior e o menor vetor

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vetor4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int [] valores ={3, 15, 98, 10, 0, 78, 45, 13};
+           int maior = valores[0];
+                for(int x = 1 ; x < valores.Length ; x++ ) { 
+                    if (valores[x] > maior){
+                        maior = valores[x];
+            }
+                }           
+            Console.WriteLine ("O maior valor do vetor é: " +maior);
+
+            int menor = valores[0];
+                for(int x = 1 ; x < valores.Length ; x++ ) { 
+                    if (valores[x] < menor){
+                        menor = valores[x];
+            }
+                }           
+            Console.WriteLine ("O menor valor do vetor é: " +menor);
+
+
+        }
+    }
+}
